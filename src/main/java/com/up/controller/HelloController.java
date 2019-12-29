9659dcdec7f115d42b29d6051422f1f3d65f7a65
anan_global <1145257602@qq.com>
2019-12-29 18:19:42 +0800

    项目经理1

--- src/main/java/com/up/controller/HelloController.java
+++ src/main/java/com/up/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return  "hello git 羡慕经理";
+        return  "hello git 羡慕经理123";
     }
 
     //我是开发人员，我编写了
